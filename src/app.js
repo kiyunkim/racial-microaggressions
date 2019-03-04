564219f44ce4da9695750ac8168944536138c95e
K <kiyunkim@users.noreply.github.com>
2019-03-04 13:50:34 -0500

    webpack

---
+++ src/app.js
@@ -0,0 +1,5 @@
+import url from '../src/style.css';
+
+
+
+// alert('aaa');
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+ module.exports = {
+   devtool: 'cheap-module-eval-source-map',
+   context: path.join(__dirname, 'src'),
+   entry: {
+     app: path.join(__dirname, 'src', 'app.js')
+   },
+   output: {
+     path: path.join(__dirname, 'dist'),
+     filename: 'bundle.js'
+   },
+   module: {
+     rules: [
+       {
+         test: /\.js$/,
+         include: /src/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-transform-runtime']
+            }
+          }
+       },
+       {
+         test: /\.css$/,
+         include: /src/,
+         use: ['style-loader/url', 'file-loader']
+       }
+     ]
+   },
+   devServer: {
+     contentBase: path.join(__dirname, 'dist'),
+     inline: true // dont run my app inside the webpack iframe
+   },
+   plugins: [
+     new htmlWebpackPlugin({
+       title: 'title',
+       header: 'header',
+       template: path.join(__dirname, 'src', 'index.html'),
+       hash: true,
+       chunks: ['app'] // name of the bundle, determined by the key in line 6. add only this bundle
+     })
+   ]
+ }
